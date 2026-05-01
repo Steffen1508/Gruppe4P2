fef99053878f73a12f82ccdc9106c87b66f924ea
021021-dani <danielparhizkari@gmail.com>
2026-05-01 13:01:20 +0200

    new push

--- PII.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-# Login using e.g. `huggingface-cli login` to access this dataset
-df = pd.read_parquet("hf://datasets/syvai/pii-dataset-eng/data/train-00000-of-00001.parquet")
-print(df.head())
-df.info()
